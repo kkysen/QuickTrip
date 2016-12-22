597859fecdce7d53c2aea4fdac4623b52b6f3141
Khyber Sen <kkysen@gmail.com>
2016-12-22 18:59:01 -0500

    Update GoogleApiRequest.java

--- src/main/java/io/github/kkysen/quicktrip/apis/google/GoogleApiRequest.java
+++ src/main/java/io/github/kkysen/quicktrip/apis/google/GoogleApiRequest.java
@@ -1,1 +1,19 @@
-public abstract class GoogleApiRequest<T> {}
+public abstract class GoogleMapsApiRequest<T> extends JsonApiRequest<T> {
+    
+    private static final String API_KEY = "AIzaSyBrc16mMFU7w8Hyo7nFD6ny5SjeOapkY9Q";
+    
+    private static final String URL = "https://maps.googleapis.com/maps/api/$REQUEST_TYPE/json";
+    
+    protected String getApiKey() {
+        return API_KEY;
+    }
+    
+    protected String getBaseUrl() {
+        return URL.replace("$REQUEST_TYPE", getRequestType());
+    }
+    
+    protected abstract String getRequestType();
+    
+    
+    
+}

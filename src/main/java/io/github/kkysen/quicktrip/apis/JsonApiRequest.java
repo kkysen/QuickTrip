f8e123ffc930ac6a90d6570f2b670bc1234bdada
Khyber Sen <kkysen@gmail.com>
2016-12-22 11:47:53 -0500

    Update JsonApiRequest.java

--- src/main/java/io/github/kkysen/quicktrip/apis/JsonApiRequest.java
+++ src/main/java/io/github/kkysen/quicktrip/apis/JsonApiRequest.java
@@ -1,1 +1,9 @@
-public abstract class JsonApiRequest<T> extends ApiRequest<T> {}
+public abstract class JsonApiRequest<T> extends ApiRequest<T> {
+    
+    private static final Gson gson = new Gson();
+    
+    protected T parseRequest(Reader reader) {
+        return fromJson(reader);
+    }
+    
+}

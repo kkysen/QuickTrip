c799a9a8b57d95e8c7420943c7a3f614ea071343
Khyber Sen <kkysen@gmail.com>
2016-12-22 19:38:04 -0500

    inherited, documented Json annotation

---
+++ src/main/java/io/github/kkysen/quicktrip/apis/Json.java
@@ -0,0 +1,20 @@
+package io.github.kkysen.quicktrip.apis;
+
+import static java.lang.annotation.ElementType.TYPE;
+import static java.lang.annotation.RetentionPolicy.SOURCE;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Inherited;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+/**
+ * 
+ * 
+ * @author Khyber Sen
+ */
+@Inherited
+@Documented
+@Retention(SOURCE)
+@Target(TYPE)
+public @interface Json {}
--- src/main/java/io/github/kkysen/quicktrip/apis/google/maps/directions/Directions.java
+++ src/main/java/io/github/kkysen/quicktrip/apis/google/maps/directions/Directions.java
@@ -1,11 +1,13 @@
 package io.github.kkysen.quicktrip.apis.google.maps.directions;
 
+import io.github.kkysen.quicktrip.apis.Json;
 
 /**
  * 
  * 
  * @author Khyber Sen
  */
+@Json
 public class Directions {
     
 }

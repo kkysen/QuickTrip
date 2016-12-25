166f05312441279c6e9219e087f3be8e1b90f10e
Khyber Sen <kkysen@gmail.com>
2016-12-25 01:14:11 -0500

    began to create input GUI

--- src/main/java/io/github/kkysen/quicktrip/core/QuickTrip.java
+++ src/main/java/io/github/kkysen/quicktrip/core/QuickTrip.java
@@ -1,9 +1,18 @@
 package io.github.kkysen.quicktrip.core;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 /**
@@ -13,19 +22,59 @@ import javafx.stage.Stage;
  */
 public class QuickTrip extends Application {
     
+    private static final String TITLE = "Quick Trip";
+    
+    private TextField createDestination(final int destNum, final GridPane grid, final int rowIndex) {
+        final Label destLabel = new Label("Destination " + destNum);
+        grid.add(destLabel, 0, rowIndex);
+        
+        final TextField dest = new TextField();
+        grid.add(dest, 1, rowIndex);
+        return dest;
+    }
+    
+    private List<TextField> createDestinations(int numDests, final GridPane grid, int startRowIndex) {
+        numDests++;
+        startRowIndex--;
+        final List<TextField> destinations = new ArrayList<>(numDests);
+        for (int destNum = 1; destNum < numDests; destNum++) {
+            destinations.add(createDestination(destNum, grid, startRowIndex + destNum));
+        }
+        return destinations;
+    }
+    
     @Override
     public void start(final Stage primaryStage) {
-        final Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(event -> System.out.println("Hello World!"));
         
-        final StackPane root = new StackPane();
-        root.getChildren().add(btn);
+        final GridPane grid = new GridPane();
+        grid.setAlignment(Pos.CENTER);
+        grid.setHgap(10);
+        grid.setVgap(10);
+        grid.setPadding(new Insets(25, 25, 25, 25));
         
-        final Scene scene = new Scene(root, 300, 250);
+        final Scene scene = new Scene(grid, 300, 250);
         
-        primaryStage.setTitle("Quick Trip");
+        primaryStage.setTitle(TITLE);
         primaryStage.setScene(scene);
+        
+        final Text welcomeMsg = new Text("Welcome to " + TITLE);
+        welcomeMsg.setFont(Font.font("Tahoma", FontWeight.NORMAL, 20));
+        grid.add(welcomeMsg, 0, 0, 2, 1);
+        
+        final Label originLabel = new Label("Origin");
+        grid.add(originLabel, 0, 1);
+        
+        final TextField origin = new TextField();
+        grid.add(origin, 1, 1);
+        
+//        final Label destinationLabel = new Label("Destination");
+//        grid.add(destinationLabel, 0, 2);
+//        
+//        final TextField destination = new TextField();
+//        grid.add(destination, 1, 2);
+        
+        final List<TextField> destinations = createDestinations(5, grid, 2);
+        
         primaryStage.show();
     }
     

07f749a0c5ac171b16200578fa6ad2ffa9508d5f
Stanley Lin <slin55055@gmail.com>
2016-12-28 20:33:40 -0500

    Added okhttp dependency

--- src/main/java/io/github/kkysen/quicktrip/apis/skyscanner/SkyscannerApiRequest.java
+++ src/main/java/io/github/kkysen/quicktrip/apis/skyscanner/SkyscannerApiRequest.java
@@ -14,7 +14,10 @@ import java.util.List;
 public abstract class SkyscannerApiRequest<R> extends JsonApiRequest<R> {
     
     // FIXME not sure if API key is different for flights, hotels, etc. or just one key for everything
-    private static final String API_KEY = "st671638446256855850209861703997";
+    //private static final String API_KEY = "st671638446256855850209861703997";
+	
+	//The one they use for testing, might work?
+	private static final String API_KEY = "prtl6749387986743898559646983194";
     
     private static final String BASE_URL = "http://partners.api.skyscanner.net/apiservices/";
     

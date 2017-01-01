24153fb8d3571e42ba513bb1ecceef18ce08dc1c
Khyber Sen <kkysen@gmail.com>
2017-01-01 02:23:55 -0500

    destroy those 'm's

--- src/main/java/io/github/kkysen/quicktrip/apis/skyscanner/hotels/details/HotelDetailsApiRequest.java
+++ src/main/java/io/github/kkysen/quicktrip/apis/skyscanner/hotels/details/HotelDetailsApiRequest.java
@@ -1,71 +1,72 @@
 package io.github.kkysen.quicktrip.apis.skyscanner.hotels.details;
 
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.GregorianCalendar;
-import java.util.List;
-import java.util.Locale;
-
 import io.github.kkysen.quicktrip.apis.skyscanner.SkyscannerApiRequest;
 import io.github.kkysen.quicktrip.apis.skyscanner.hotels.HotelPricesApiRequest;
 import io.github.kkysen.quicktrip.apis.skyscanner.hotels.details.response.HotelDetailsResponse;
 import io.github.kkysen.quicktrip.apis.skyscanner.hotels.prices.response.HotelEntry;
 import io.github.kkysen.quicktrip.apis.skyscanner.hotels.prices.response.HotelResponse;
-import lombok.AllArgsConstructor;
 
-//@AllArgsConstructor
-public class HotelDetailsApiRequest extends SkyscannerApiRequest<HotelDetailsResponse>{
-	private String mRestOfUrl;
-	private List<String> mHotelIds;
-	
-	public HotelDetailsApiRequest(String rest, List<String> ids) {
-		System.out.println("con: " + ids == null);
-		mRestOfUrl = rest;
-		mHotelIds = new ArrayList<>(ids);
-	}
-	
-	//Ask how to use this thing
-	@Override
-	protected List<String> getUrlPathParts() {
-		/*List<String> res = new ArrayList<>();
-		res.add(mRestOfUrl);
-		System.out.println(mHotelIds == null);
-		res.addAll(mHotelIds);
-		return res;*/
-		return null;
-	}
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.GregorianCalendar;
+import java.util.List;
+import java.util.Locale;
+import java.util.stream.Collectors;
 
-	@Override
-	protected Class<? extends HotelDetailsResponse> getPojoClass() {
-		return HotelDetailsResponse.class;
-	}
-	
-	public static void main(String[] args) throws IOException {
-		HotelResponse h = (new HotelPricesApiRequest(
-    			"US",
-    			Locale.US,
-    			40.71, -74.00,
-    			new GregorianCalendar(2017, 0, 5).toZonedDateTime(),
-    			new GregorianCalendar(2017, 0, 6).toZonedDateTime(),
-    			2,
-    			1)).call();
-		
-		List<String> f = Arrays.asList(h.getMHotelList()
-				.stream()
-				.map(t -> ""+t.getMId())
-				.toArray(String[]::new));
-		/*HotelDetailsApiRequest r = new HotelDetailsApiRequest(
-				h.getMHotelUrl().getMDetails(),
-				Arrays.asList(h.getMHotelList()
-						.stream()
-						.map(t -> ""+t.getMId())
-						.toArray(String[]::new))
-				);*/
-		HotelDetailsApiRequest r = new HotelDetailsApiRequest(
-				h.getMHotelUrl().getMDetails(),
-				f);
-		System.out.println(f.get(0));
-		//System.out.println(r.mHotelIds.get(0));
-	}
+// @AllArgsConstructor
+public class HotelDetailsApiRequest extends SkyscannerApiRequest<HotelDetailsResponse> {
+    
+    private final String restOfUrl;
+    private final List<String> hotelIds;
+    
+    public HotelDetailsApiRequest(final String restOfUrl, final List<String> ids) {
+        System.out.println("con: " + ids == null);
+        this.restOfUrl = restOfUrl;
+        hotelIds = new ArrayList<>(ids);
+    }
+    
+    //Ask how to use this thing
+    @Override
+    protected List<String> getUrlPathParts() {
+        /*List<String> res = new ArrayList<>();
+        res.add(restOfUrl);
+        System.out.println(hotelIds == null);
+        res.addAll(hotelIds);
+        return res;*/
+        return null;
+    }
+    
+    @Override
+    protected Class<? extends HotelDetailsResponse> getPojoClass() {
+        return HotelDetailsResponse.class;
+    }
+    
+    public static void main(final String[] args) throws IOException {
+        final HotelResponse h = new HotelPricesApiRequest(
+                "US",
+                Locale.US,
+                40.71, -74.00,
+                new GregorianCalendar(2017, 0, 5).toZonedDateTime(),
+                new GregorianCalendar(2017, 0, 6).toZonedDateTime(),
+                2,
+                1).call();
+        
+        final List<String> f = h.getHotelList()
+                .stream()
+                .map(HotelEntry::getId)
+                .map(Object::toString)
+                .collect(Collectors.toList());
+        /*HotelDetailsApiRequest r = new HotelDetailsApiRequest(
+        		h.getMHotelUrl().getMDetails(),
+        		Arrays.asList(h.getMHotelList()
+        				.stream()
+        				.map(t -> ""+t.getMId())
+        				.toArray(String[]::new))
+        		);*/
+        final HotelDetailsApiRequest r = new HotelDetailsApiRequest(
+                h.getHotelUrl().getDetails(),
+                f);
+        System.out.println(f.get(0));
+        //System.out.println(r.mHotelIds.get(0));
+    }
 }
--- src/main/java/io/github/kkysen/quicktrip/apis/skyscanner/hotels/details/response/HotelDetailsResponse.java
+++ src/main/java/io/github/kkysen/quicktrip/apis/skyscanner/hotels/details/response/HotelDetailsResponse.java
@@ -1,15 +1,12 @@
 package io.github.kkysen.quicktrip.apis.skyscanner.hotels.details.response;
 
+import io.github.kkysen.quicktrip.apis.Json;
+import io.github.kkysen.quicktrip.apis.skyscanner.hotels.prices.response.HotelEntry;
+
 import java.util.List;
 
 import com.google.gson.annotations.SerializedName;
 
-import io.github.kkysen.quicktrip.apis.Json;
-import io.github.kkysen.quicktrip.apis.skyscanner.hotels.prices.response.Amenity;
-import io.github.kkysen.quicktrip.apis.skyscanner.hotels.prices.response.HotelEntry;
-import io.github.kkysen.quicktrip.apis.skyscanner.hotels.prices.response.HotelPrice;
-import io.github.kkysen.quicktrip.apis.skyscanner.hotels.prices.response.HotelUrl;
-import io.github.kkysen.quicktrip.apis.skyscanner.hotels.prices.response.Place;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -26,6 +23,8 @@ import lombok.Setter;
 @Getter
 @Setter
 public class HotelDetailsResponse {
-	@SerializedName("hotels")
-	private List<HotelEntry> mHotels;
+    
+    @SerializedName("hotels")
+    private List<HotelEntry> hotels;
+    
 }

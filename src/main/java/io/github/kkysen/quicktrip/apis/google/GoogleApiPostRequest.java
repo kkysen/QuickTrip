4fe7f10fe693b9d2c26dcd5fcb40441a4e3c4260
Khyber Sen <kkysen@gmail.com>
2017-01-04 11:34:04 -0600

    Update GoogleApiPostRequest.java

--- src/main/java/io/github/kkysen/quicktrip/apis/google/GoogleApiPostRequest.java
+++ src/main/java/io/github/kkysen/quicktrip/apis/google/GoogleApiPostRequest.java
@@ -1,1 +1,23 @@
 
+public abstract class GoogleApiPostRequest<P, R> extends GoogleApiRequest<R> {
+    
+    protected abstract P getRequest();
+    
+    protected abstract Class<? extends P> getRequestClass();
+    
+    protected Type getRequestType() {
+        return new TypeToken.of(getRequestClass()).getType());
+    }  
+    
+    protected R deserializeFromUrl(String url) {
+        
+        HttpPost request = new HttpPost(url);
+        request.addHeader("Content-type", "application/json");
+        request.addHeader("Accept", "application/json");
+        StringEntity params = new StringEntity(GSON.toJson(getRequest(), getRequestType()));
+        request.setEntity(params);
+        HttpClient httpClient = HttpClientBuilder.create().build();
+        HttpResponse response = httpClient.execute(request);
+    }
+    
+}

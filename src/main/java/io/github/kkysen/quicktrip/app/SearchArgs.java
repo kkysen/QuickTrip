065124809d2d267a6805984aa033b63d3f3f3f43
Khyber Sen <kkysen@gmail.com>
2017-01-06 20:55:37 -0500

    started turning into Model for SearchScreen

--- src/main/java/io/github/kkysen/quicktrip/app/SearchArgs.java
+++ src/main/java/io/github/kkysen/quicktrip/app/SearchArgs.java
@@ -22,10 +22,28 @@ import lombok.Setter;
 @Setter
 public class SearchArgs {
     
+    private transient TextField originInput;
+    private transient DatePicker dateInput;
+    private transient List<DestField> dest
+    
     private String origin;
     private LocalDate date; // FIXME turn into joda Date
     private List<NoDateDestination> destinations;
     private int budget;
     private int numPeople;
+    
+    public void serialize() throws InputError {
+        origin = serializeOrigin();
+        date = serializeStartDate();
+        destinations = serializeDests();
+        numPeople = serializeNumPeople();
+        budget = serializeBudget();
+        final String json = QuickTripConstants.GSON.toJson(this);
+        try {
+            MyFiles.write(Paths.get(QuickTripConstants.SEARCH_ARGS_PATH), json);
+        } catch (final IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
     
 }

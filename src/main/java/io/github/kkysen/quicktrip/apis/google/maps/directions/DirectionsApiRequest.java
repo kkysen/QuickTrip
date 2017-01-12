2ba30e0150f1f8554e4d8b87db86deb800753e2d
Khyber Sen <kkysen@gmail.com>
2017-01-12 18:35:50 -0500

    fixed DirectionsApiRequest

--- src/main/java/io/github/kkysen/quicktrip/apis/google/maps/directions/DirectionsApiRequest.java
+++ src/main/java/io/github/kkysen/quicktrip/apis/google/maps/directions/DirectionsApiRequest.java
@@ -6,33 +6,44 @@ import io.github.kkysen.quicktrip.apis.google.maps.directions.response.Direction
 
 import java.io.IOException;
 import java.nio.file.Path;
-
-import lombok.RequiredArgsConstructor;
-
-// dummy class for now
-// will become a JSON POJO
+import java.util.List;
 
 /**
  * 
  * 
  * @author Khyber Sen
  */
-@RequiredArgsConstructor
 public class DirectionsApiRequest extends GoogleMapsApiRequest<Directions> {
     
-    // FIXME
-    // these are Strings for now just so I can work on the rest
-    // should be changed to appropriate types later
-    // and QueryField#encode set to false if they are weird non-string good formatty types
-    
-    // FIXME
-    // must add correct query field names for some of these
     private final @QueryField String origin;
     private final @QueryField String destination;
-    private final @QueryField String mode;
+    private final @QueryField String mode = "driving";
     private final @QueryField String waypoints;
-    private final @QueryField String departureTime;
-    private final @QueryField String arrivalTime;
+    private final @QueryField String units = "imperial";
+    
+    public DirectionsApiRequest(final String origin, final String destination,
+            final List<String> waypoints, final boolean optimize) {
+        this.origin = origin;
+        this.destination = destination;
+        if (optimize) {
+            waypoints.add(0, "optimize:true");
+        }
+        this.waypoints = String.join("|", waypoints);
+    }
+    
+    public DirectionsApiRequest(final String origin, final String destination,
+            final List<String> waypoints) {
+        this(origin, destination, waypoints, true);
+    }
+    
+    public DirectionsApiRequest(final String origin, final List<String> waypoints,
+            final boolean optimize) {
+        this(origin, origin, waypoints, optimize);
+    }
+    
+    public DirectionsApiRequest(final String origin, final List<String> waypoints) {
+        this(origin, origin, waypoints, true);
+    }
     
     @Override
     protected String getMapsRequestType() {
@@ -54,49 +65,9 @@ public class DirectionsApiRequest extends GoogleMapsApiRequest<Directions> {
         return super.getRelativeCachePath().resolve("directions");
     }
     
-    public static Directions request(final String origin, final String destination, final String mode,
-            final String waypoints, final String departureTime, final String arrivalTime) throws IOException {
-        return new DirectionsApiRequest(origin, destination, mode, waypoints, departureTime, arrivalTime).getResponse();
-    }
-    
-    public static void main(final String[] args) throws Exception {
-        request("75 9th Ave, New York, NY",
-                "MetLife Stadium Dr East Rutherford, NJ 0703",
-                "driving",
-                "",
-                "",
-                "");
-        
-        
-        request("296 6th St, Brooklyn, NY",
-                "15 Claremont Ave, New York, NY",
-                "driving",
-                "",
-                "",
-                "");
-        
-        request("75 9th Ave, New York, NY",
-                "MetLife Stadium Dr East Rutherford, NJ 0703",
-                "driving",
-                "optimize:true|San Francisco,+CA",
-                "",
-                "");
-        
-        final String[] waypoints = {
-            "optimize:true",
-            "San Francisco,+CA",
-            "Seattle,+WA",
-            "Los Angeles,+CA",
-            "Austin,+TX",
-            "Orlando,+FL",
-            "Chicago,+IL"
-        };
-        request("296 6th St, Brooklyn, NY",
-                "15 Claremont Ave, New York, NY",
-                "driving",
-                String.join("|", waypoints),
-                "",
-                "");
+    public static Directions request(final String origin, final List<String> waypoints)
+            throws IOException {
+        return new DirectionsApiRequest(origin, waypoints).getResponse();
     }
     
 }

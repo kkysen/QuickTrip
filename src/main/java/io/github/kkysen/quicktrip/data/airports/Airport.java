42a06159efdc065eaae19fe3faa26e18fe37fb57
Khyber Sen <kkysen@gmail.com>
2017-01-13 03:10:23 -0500

    airport classification using hashmaps and all fields

---
+++ src/main/java/io/github/kkysen/quicktrip/data/airports/Airport.java
@@ -0,0 +1,66 @@
+package io.github.kkysen.quicktrip.data.airports;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+/**
+ * 
+ * 
+ * @author Khyber Sen
+ */
+@Getter
+@RequiredArgsConstructor
+public class Airport {
+        
+    private final String name;
+    private final String latitude;
+    private final String longitude;
+    private final String elevation;
+    private final String continent;
+    private final String country;
+    private final String region;
+    private final String municipality;
+    private final String gpsCode;
+    private final String iataCode;
+    private final String localCode;
+    
+    public Airport(final String[] splitCsv) {
+        this(splitCsv[0], splitCsv[1], splitCsv[2], splitCsv[3], splitCsv[4], splitCsv[5],
+                splitCsv[6], splitCsv[7], splitCsv[8], splitCsv[9], splitCsv[10]);
+    }
+    
+    private static String[] splitQuotedCsv(final String csv) {
+        final List<String> parts = new ArrayList<>();
+        int lastIndex = 0;
+        boolean inQuote = false;
+        for (int i = 0; i < csv.length(); i++) {
+            final char c = csv.charAt(i);
+            if (c == '"') {
+                inQuote = !inQuote;
+            } else {
+                if (!inQuote && c == ',') {
+                    parts.add(csv.substring(lastIndex, i));
+                    lastIndex = i + 1;
+                }
+            }
+        }
+        parts.add(csv.substring(lastIndex));
+        return parts.toArray(new String[parts.size()]);
+    }
+    
+    public Airport(final String csvLine) {
+        this(splitQuotedCsv(csvLine));
+    }
+    
+    public static void main(final String[] args) {
+        final String s = "\"\"\"Der Dingel\"\" Airfield\",51.536,9.3805,718,EU,DE,DE-HE,Hofgeismar,,,";
+        System.out.println(splitQuotedCsv(s).length);
+        System.out.println(Arrays.toString(splitQuotedCsv(s)));
+    } 
+    
+    
+}
--- src/main/java/io/github/kkysen/quicktrip/data/airports/Airports.java
+++ src/main/java/io/github/kkysen/quicktrip/data/airports/Airports.java
@@ -1,13 +1,19 @@
 package io.github.kkysen.quicktrip.data.airports;
 
+import io.github.kkysen.quicktrip.Constants;
 import io.github.kkysen.quicktrip.io.MyFiles;
 
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 /**
@@ -17,14 +23,15 @@ import java.util.stream.Collectors;
  */
 public class Airports {
     
+    private static final Path DIR = Paths.get("src", "main", "resources", "airports");
+    
     private static String removeFirstCsv(final String csv) {
         return csv.substring(csv.indexOf(',') + 1);
     }
     
     public static void filter() throws IOException {
-        final Path dir = Paths.get("src", "main", "resources", "airports");
         final Map<String, List<String>> airportsByType = //
-                Files.lines(dir.resolve("airport-codes.csv"))
+                Files.lines(DIR.resolve("airport-codes.csv"))
                         .map(Airports::removeFirstCsv)
                         .filter(line -> line.startsWith("small")
                                 || line.startsWith("medium")
@@ -37,9 +44,8 @@ public class Airports {
         });
         airportsByType.forEach((type, airports) -> {
             try {
-                MyFiles.write(dir.resolve(type + "Airports.csv"), airports);
+                MyFiles.write(DIR.resolve(type + "Airports.csv"), airports);
             } catch (final IOException e) {
-                // TODO Auto-generated catch block
                 e.printStackTrace();
             }
         });
@@ -47,8 +53,71 @@ public class Airports {
                 .forEach(System.out::println);
     }
     
+    public static enum Type {
+        SMALL,
+        MEDIUM,
+        LARGE
+    }
+    
+    private static final List<Type> TYPES = new ArrayList<>(
+            Arrays.asList(Type.values()));
+    private static final List<Function<Airport, String>> CLASSIFIERS = new ArrayList<>();
+    static {
+        CLASSIFIERS.add(Airport::getName);
+        CLASSIFIERS.add(Airport::getLatitude);
+        CLASSIFIERS.add(Airport::getLongitude);
+        CLASSIFIERS.add(Airport::getElevation);
+        CLASSIFIERS.add(Airport::getContinent);
+        CLASSIFIERS.add(Airport::getCountry);
+        CLASSIFIERS.add(Airport::getRegion);
+        CLASSIFIERS.add(Airport::getMunicipality);
+        CLASSIFIERS.add(Airport::getGpsCode);
+        CLASSIFIERS.add(Airport::getIataCode);
+        CLASSIFIERS.add(Airport::getLocalCode);
+    }
+    
+    private final Map<Type, Map<Function<Airport, String>, Map<String, List<Airport>>>> airports;
+    
+    private static Map<Function<Airport, String>, Map<String, List<Airport>>> loadAirports(
+            final Path path) throws IOException {
+        final List<Airport> airports = Files.lines(path, Constants.CHARSET)
+                //.peek(System.out::println)
+                .map(Airport::new)
+                .collect(Collectors.toList());
+        final Map<Function<Airport, String>, Map<String, List<Airport>>> classifiedAirports = new HashMap<>();
+        CLASSIFIERS.stream()
+                .forEach(classifier -> {
+                    final Map<String, List<Airport>> classification = airports.stream()
+                            .collect(Collectors.groupingBy(classifier));
+                    classifiedAirports.put(classifier, classification);
+                });
+        return classifiedAirports;
+    }
+    
+    public Airports() {
+        airports = new ConcurrentHashMap<>(TYPES.size());
+        TYPES.parallelStream()
+                .forEach(type -> {
+                    final Path path = DIR.resolve(type.toString().toLowerCase() + "Airports.csv");
+                    Map<Function<Airport, String>, Map<String, List<Airport>>> loadedAirports;
+                    try {
+                        loadedAirports = loadAirports(path);
+                    } catch (final IOException e) {
+                        e.printStackTrace();
+                        return;
+                    }
+                    airports.put(type, loadedAirports);
+                });
+    }
+    
+    @Override
+    public String toString() {
+        return airports.toString();
+    }
+    
     public static void main(final String[] args) throws IOException {
-        filter();
+        final Airports airports = new Airports();
+        System.out.println(airports);
     }
     
 }
